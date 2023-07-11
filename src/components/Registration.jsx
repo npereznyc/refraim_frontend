73605876cfeb809b12b18a89ed50350aeaeb9f30
npereznyc <natalieperez84@gmail.com>
2023-07-11 16:27:29 -0400

    created registartion form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,168 +1,84 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { TextField } from 'formik-material-ui';
+import { Button } from '@mui/material';
+import {Formik, Field, Form} from 'formik'
 
 export default function Registration() {
   return (
-    <Box
+    <Formik
+      initialValues={{ 
+        username: '', 
+        email: '', 
+        password: '', 
+        firstName: '',
+        lastName: ''
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          console.log(values);
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ submitForm, isSubmitting }) => (
+    <Form>
+       <Box
       component="form"
       sx={{
         '& .MuiTextField-root': { m: 1, width: '25ch' },
       }}
-      noValidate
-      autoComplete="off"
+    //   noValidate
+    //   autoComplete="off"
     >
       <div>
-        <TextField
+        <Field
           required
           id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
+          component={TextField}
+          name='username'
+          type='text'
+          label="Username"
         />
-        <TextField
-          disabled
+        <Field
+          required
           id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
+          component={TextField}
+          name='email'
+          label="Email"
+          type="email"
         />
-        <TextField
+        <Field
           id="outlined-password-input"
+          component={TextField}
+          name='password'
           label="Password"
           type="password"
           autoComplete="current-password"
         />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
+        <Field
+          id="outlined-fist-name-input"
+          component={TextField}
+          name='firstName'
+          label="First Name"
         />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
+        <Field
+          id="outlined-last-name-input"
+          component={TextField}
+          name='lastName'
+          label="Last Name"
         />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
+        <Button 
+        type="submit"
+        variant="contained" 
+        color="primary" 
+        style={{ marginTop: '1em' }}>
+        Register
+      </Button>
       </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="standard-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          disabled
-          id="standard-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="standard"
-        />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-        />
-        <TextField
-          id="standard-search"
-          label="Search field"
-          type="search"
-          variant="standard"
-        />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="standard"
-        />
-      </div>
-    </Box>
+    </Box> 
+    </Form>
+      )}
+    </Formik>
   );
 }

3490ff3c4a2615a0465a6a2475cf98b81055c56e
Jordan-Burgess <alfredjordan93@gmail.com>
2023-07-21 14:22:09 -0400

    Add Date field to RefraimSession

--- src/pages/RefraimSession.jsx
+++ src/pages/RefraimSession.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import ConversationBox from "../components/ConversationBox";
 import { Typography } from "@mui/material";
+import dateFormat from 'dateformat'
 
 
 function RefraimSession() {
-
+    const now = new Date();
     return (
         <section id="convo-container">
             <div className="setup-inner">
                 <div className="refraim-prompt" id="refraim-prompt">
+                    <p>{dateFormat(now, "mmmm dS")}</p>
                     <Typography variant='body1' id="convo-text">
                         Life can be tough. What negative thoughts are you having?
                     </Typography>

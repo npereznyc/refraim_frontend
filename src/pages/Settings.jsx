0b46c392c702fde54b60621f9753908abc0b60a9
Jordan-Burgess <alfredjordan93@gmail.com>
2023-08-02 15:09:58 -0400

    Add Logout button to settings page

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Button } from '@mui/material';
 import {Typography} from '@mui/material';
 import Nav from '../components/Nav';
+import AuthContext from '../auth';
 
 function Settings() {
+    let {logout} = useContext(AuthContext)
     return(
         <>
             <p>Settings Page</p>
+            <Button
+                variant="contained"
+                onClick={logout}>Logout</Button>
             <Nav/>
         </>
     )

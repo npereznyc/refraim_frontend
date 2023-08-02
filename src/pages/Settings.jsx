001388dbe5a72a5dc62b881d155ee99bd2158026
Jordan-Burgess <alfredjordan93@gmail.com>
2023-08-02 14:43:52 -0400

    Create Settings page boiler plate

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import {Typography} from '@mui/material';
+import Nav from '../components/Nav';
+
+function Settings() {
+    return(
+        <p>Settings Page</p>
+    )
+}
+
+export default Settings;

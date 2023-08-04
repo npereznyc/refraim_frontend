d710c9f05edeb75a3e3c8258944c133323851c06
npereznyc <natalieperez84@gmail.com>
2023-08-04 16:32:18 -0400

    Added brain icon to RefraimSession page

--- src/pages/RefraimSession.jsx
+++ src/pages/RefraimSession.jsx
@@ -3,20 +3,25 @@ import ConversationBox from "../components/ConversationBox";
 import { Typography } from "@mui/material";
 import dateFormat from 'dateformat'
 import Nav from "../components/Nav";
+import Brain_Chat from '../assets/Brain_Chat.png'
 
 function RefraimSession() {
     const now = new Date();
     return (
         <section id="convo-container">
-            <div className="setup-inner">
-                <div className="refraim-prompt" id="refraim-prompt">
-                    <Typography className='date' variant='body1' id="convo-text">
+            <div>
+                <Typography className='date' variant='body1'>
                         {dateFormat(now, "mmmm dS")}
                     </Typography>
                     <br />
+                <div className="refraim-prompt" id="refraim-prompt">
+                    
+                    <img className='brain-icon' src={Brain_Chat} />
+             
                     <Typography variant='body1' id="convo-text">
                         Life can be tough. What negative thoughts are you having?
                     </Typography>
+                    
                 </div>
             </div>
             <ConversationBox />

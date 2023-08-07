d59830bdd39e88f43dbd9be575e297b516b20a73
npereznyc <natalieperez84@gmail.com>
2023-08-07 17:21:20 -0400

    Added message on Favorites page if the user has not saved any favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,17 +1,22 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import AuthContext from '../auth';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 import Nav from '../components/Nav';
 import Like from '../components/Like';
+import { Button } from '@mui/material';
+import {useNavigate} from 'react-router-dom'
 
-const API_URL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:8000' // Your local Django server's URL
-  : 'https://refraim-backend-e8c71717cd42.herokuapp.com'; // Your deployed Django server's URL
+const API_URL = process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000' // Your local Django server's URL
+    : 'https://refraim-backend-e8c71717cd42.herokuapp.com'; // Your deployed Django server's URL
 
 function Favorites() {
 
     const [conversations, setConversations] = useState([])
-    let {user} = useContext(AuthContext)
+    let { user } = useContext(AuthContext)
+    const navigate = useNavigate()
+    const isLoggedIn = user !== null && user !== undefined;
+    
 
     useEffect(() => {
         fetchFavorites()
@@ -33,20 +38,50 @@ function Favorites() {
         }
     }
 
+    const handleButtonClick = () => {
+        if(isLoggedIn) {
+            navigate('/pre-prompt')
+        } else {
+            navigate('/welcome')
+        }
+    }
+
     return (
         <div className='history-section'>
             <Typography variant="h1" color='primary' >Favorites</Typography>
-            {conversations.reverse().map(conversation => (
+            <br />
+            {conversations.length < 1 ? (
+                <>
+                <Typography variant='body1' id="convo-text">
+                    You haven't saved any Refraims to your favorites yet! <br />
+                </Typography>
+                <br />
+                <Typography variant='body1' id="convo-text">
+                Start a new Refraim Session: <br />
+            </Typography>
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                onClick={handleButtonClick}
+                style={{ marginTop: '1em' }} >
+                Begin
+            </Button>
+            </>
+                
+            ) : conversations.reverse().map(conversation => (
                 <div className='history' key={conversation.id}>
                     <Typography variant='h3'>{new Date(conversation.created_at).toLocaleString()}</Typography>
                     <br />
                     <Typography className='convo-text' variant='body1'>Negative Thought: {conversation.prompt}</Typography>
                     <br />
                     <Typography className='convo-text' variant='body1'>Positive Refraim: {conversation.conclusion} </Typography>
-                    <Like className='like-button' conversationId={conversation.id} initialFavorite={conversation.is_favorite}/>
+                    <Like className='like-button' conversationId={conversation.id} initialFavorite={conversation.is_favorite} />
                     <hr />
                 </div>
-            ))}
+            ))
+            }
+
             <br />
             <Nav />
         </div>

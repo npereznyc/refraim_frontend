b36d6bbdda1dbfa372e60250963ff2ba61ffe1f4
npereznyc <natalieperez84@gmail.com>
2023-08-14 16:51:39 -0400

    Added logic to Home.jsx to render the user's favorite refraims. Needs styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, CardContent, Typography, CardActions, Card } from '@mui/material';
 import { useNavigate } from 'react-router-dom'
 import AuthContext from '../auth';
 import SwipeableViews from 'react-swipeable-views';
+import useFavorites from '../components/useFavorites';
+import Nav from '../components/Nav';
 
 
 function Home() {
 
     let { user } = useContext(AuthContext)
-    const navigate = useNavigate()
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const { favorites, loading } = useFavorites(user.user_id || user.id)
+
+    const handleChangeIndex = index => {
+        setCurrentIndex(index);
+    };
+
     // const isLoggedIn = user !== null && user !== undefined;
 
     // const handleButtonClick = () => {
@@ -23,7 +31,39 @@ function Home() {
             <Typography variant='h1' color="primary" gutterBottom>
                 Welcome!
             </Typography>
+
             <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography variant='h2' color="primary" gutterBottom>
+                        My Favorite Refraims
+                    </Typography>
+
+                    <SwipeableViews
+                        index={currentIndex}
+                        onChangeIndex={(index) => setCurrentIndex(index)}
+                    >
+                        {favorites.map((favorite, index) => (
+                            <div key={index}>
+                                <Typography variant="body2">
+                                    {favorite.conclusion} {/* Render the favorite */}
+                                </Typography>
+                            </div>
+                        ))}
+                    </SwipeableViews>
+                    <Button disabled={currentIndex === 0} onClick={() => handleChangeIndex(currentIndex - 1)}>
+                        Previous
+                    </Button>
+                    <Button disabled={currentIndex === favorites.length - 1} onClick={() => handleChangeIndex(currentIndex + 1)}>
+                        Next
+                    </Button>
+                </CardContent>
+                <CardActions>
+                    {Array.from({ length: favorites.length }).map((_, index) => (
+                        <span key={index} className={index === currentIndex ? 'dot filled' : 'dot'}></span>
+                    ))}
+                </CardActions>
+            </Card>
+            {/* <Card sx={{ minWidth: 275 }}>
                 <CardContent>
                     <Typography variant='h2' color="primary" gutterBottom>
                         My Favorite Refraims
@@ -36,7 +76,7 @@ function Home() {
                 <CardActions>
                     <Button size="small">Learn More</Button>
                 </CardActions>
-            </Card>
+            </Card> */}
 
             <Card sx={{ minWidth: 275 }}>
                 <CardContent>
@@ -52,6 +92,8 @@ function Home() {
                     <Button size="small">Begin</Button>
                 </CardActions>
             </Card>
+
+            <Nav />
         </div>
     );
 }
